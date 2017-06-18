a56459e44232d8707a86a3e183366d597325d859
Kirby Banman <kirby.banman@gmail.com>
2017-06-18 00:57:39 -0600

    Finish very slow forard runner with html results summary

--- forward.py
+++ forward.py
@@ -8,12 +8,53 @@ from tensorflow.examples.tutorials.mnist import input_data
 
 FLAGS = None
 
+HTML_TEMPLATE = """
+  <html>
+  <head>
+    <style>
+    body {
+      width: auto;
+      overflow-x: scroll;
+      white-space: nowrap;
+    }
+    .row {
+      margin-bottom: 15px;
+    }
+    .padRightSmall {
+      padding-right: 5px;
+    }
+    .padRightLarge {
+      padding-right: 20px;
+    }
+    code {
+      font-size: 14px;
+      vertical-align: top;
+    }
+    </style>
+  </head>
+  <body>
+  $ROWS$
+  </body>
+  </html>
+"""
+
+ROW_TEMPLATE = """
+  <div class="row">
+    <code>In:</code><img class="padRightSmall" src="$DIR_NAME$/$INPUT_IMAGE_FILENAME$"/>
+    <code>Out:</code><img class="padRightLarge" src="$DIR_NAME$/$OUTPUT_IMAGE_FILENAME$"/>
+    <code>Animated:</code><img class="padRightLarge" src="$DIR_NAME$/$LAYER_VIDEO_FILENAME$"/>
+    <code>Frames:</code>$LAYER_IMAGES$
+  </div>
+"""
+
+LAYER_IMAGE_TEMPLATE = "<img class="padRightSmall" src="$DIR_NAME$/$LAYER_IMAGE_FILENAME$"/>"
+
 
 def get_input_image(filename):
   image = imageio.imread(filename)
   if image.shape == (28, 28, 3):
     return [image[:,:,0].reshape((784))]
-  elif image.shape == (28, 28, 1):
+  elif image.shape == (28, 28, 1) or image.shape == (28, 28):
     return [image.reshape((784))]
   else:
     raise Exception("Incompatible input image shape: " + str(image.shape))
@@ -34,12 +75,52 @@ def get_outputs(sess, input_image):
   return output_image, layer_images
 
 
-def write_outputs(input_filename, output_image, layer_images):
-  basename = os.path.basename(filename)
-  input_name = os.path.splitext(basename)[0]
+def write_result_images(input_filepath, output_image, layer_images):
+  input_image_filename = os.path.basename(input_filepath)
+  input_name_no_extension = os.path.splitext(input_image_filename)[0]
 
-  imageio.imwrite(os.path.join(FLAGS.input_dir, input_name + "_output_image.png"), output_image)
-  imageio.mimwrite(os.path.join(FLAGS.input_dir, input_name + "layer_images.gif"), layer_images, duration=0.5)
+  output_image_filename = os.path.join(FLAGS.input_dir, input_name_no_extension + "_output_image.png")
+  layer_video_filename = os.path.join(FLAGS.input_dir, input_name_no_extension + "_layer_images.gif")
+
+  imageio.imwrite(output_image_filename, output_image)
+  imageio.mimwrite(layer_video_filename, layer_images, duration=0.5)
+
+  i = 0
+  layer_image_filenames = []
+  for layer_image in layer_images:
+    layer_image_filename = os.path.join(FLAGS.input_dir, input_name_no_extension + "_layer_image_" + str(i) + ".png")
+    imageio.imwrite(layer_image_filename, layer_image)
+
+    layer_image_filenames.append(layer_image_filename)
+    i += 1
+
+  #write_html_summary(input_image_filename, layer_video_filename, output_image_filename, layer_image_filenames)
+  return {
+    "input_image_filename": input_image_filename,
+    "layer_video_filename": layer_video_filename,
+    "output_image_filename": output_image_filename,
+    "layer_image_filenames": layer_image_filenames
+  }
+
+
+def write_html_summary(result_filenames):
+  rows = []
+  for result_row_filenames in result_filenames:
+    input_image_filename = result_row_filenames["input_image_filename"]
+    layer_video_filename = result_row_filenames["layer_video_filename"]
+    output_image_filename = result_row_filenames["output_image_filename"]
+    layer_image_filenames = result_row_filenames["layer_image_filenames"]
+
+    layer_image_tags = "".join([LAYER_IMAGE_TEMPLATE.replace("$LAYER_IMAGE_FILENAME$", layer_image_filename) for layer_image_filename in layer_image_filenames])
+    row = ROW_TEMPLATE.replace("$INPUT_IMAGE_FILENAME$", input_image_filename).replace("$LAYER_VIDEO_FILENAME$", layer_video_filename).replace("$OUTPUT_IMAGE_FILENAME$", output_image_filename).replace("$LAYER_IMAGES$", layer_image_tags)
+
+    rows.append(row)
+
+  input_dir_name = os.path.basename(FLAGS.input_dir)
+  html = HTML_TEMPLATE.replace("$ROWS$", "".join(rows)).replace("$DIR_NAME$", input_dir_name)
+
+  with open(os.path.join(os.path.dirname(FLAGS.input_dir), input_dir_name + " - Results.html"), "w") as f:
+    f.write(html)
 
 
 if __name__ == '__main__':
@@ -62,9 +143,12 @@ if __name__ == '__main__':
     importer = tf.train.import_meta_graph(meta_file)
     importer.restore(sess, model_location)
 
-    for filename in sorted(glob.glob(os.path.join(FLAGS.input_dir, "*.png"))):
-      input_image = get_input_image(filename)
+    result_filenames = []
+    for input_file_path in sorted(glob.glob(os.path.join(FLAGS.input_dir, "*.png"))):
+      input_image = get_input_image(input_file_path)
 
       output_image, layer_images = get_outputs(sess, input_image)
 
-      write_outputs(filename, output_image, layer_images)
+      result_filenames.append(write_result_images(input_file_path, output_image, layer_images))
+
+    write_html_summary(result_filenames)
